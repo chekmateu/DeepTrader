781063032924ccfca5ac8ab8ba957dcccfa95c0c
chekmateu <126988953+chekmateu@users.noreply.github.com>
2023-12-31 11:40:00 -0700

    Update

--- utils.py
+++ utils.py
@@ -7,6 +7,9 @@ from bokeh.plotting import figure, show
 from bokeh.layouts import gridplot
 from bokeh.models import RangeTool, HoverTool, ColumnDataSource, CDSView, IndexFilter
 from math import pi
+import statsmodels.tsa.stattools as stats
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
 '''
 Data: ohlcv data -> pd.Dataframe
@@ -160,3 +163,166 @@ def find_missing_days(times: pl.Series) -> pl.LazyFrame:
 def getUTCUnixFromDt(year, month, day, hour, minute, second):
     # Returns ms Unix time from datetime in UTC timezone
     return time.mktime(datetime.utcfromtimestamp(time.mktime(datetime(year, month, day, hour, minute, second).timetuple())).timetuple()) * 1000
+
+'''
+AdFuller Test for stationarity of time series data
+'''
+def check_stationarity(series):
+    # Copied from https://machinelearningmastery.com/time-series-data-stationary-python/
+
+    result = stats.adfuller(series.values)
+
+    print('ADF Statistic: %f' % result[0])
+    print('p-value: %f' % result[1])
+    print('Critical Values:')
+    for key, value in result[4].items():
+        print('\t%s: %.3f' % (key, value))
+
+    if (result[1] <= 0.05) & (result[4]['5%'] > result[0]):
+        print("\u001b[32mStationary\u001b[0m")
+    else:
+        print("\x1b[31mNon-stationary\x1b[0m")
+
+def plot_ACF(data, lags = 40):
+
+    acf, _, pvalue = stats.acf(data, nlags = lags, qstat = True)
+
+    pacf = stats.pacf(data, nlags = lags)
+
+    # ACF Plot
+    p1 = figure(
+        title = 'ACF of BTC/USDT min data',
+        x_axis_label = 'Lags',
+        y_axis_label = 'Autocorrelation',
+        y_range = [-1, 1],
+        height = 400,
+        width = 800
+    )
+
+    # ACF bars
+    p1.segment(
+        x0 = list(range(0, lags + 1, 1)),
+        x1 = list(range(0, lags + 1, 1)),
+        y0 = [0] * (lags + 1),
+        y1 = acf,
+        color = 'blue',
+        line_width = 5
+    )
+
+    # At the 95% confidence interval z_(a-1)/sqrt(n - Li) a = 0.05, Li = Lag operator
+    p1.line(
+        x = list(range(0, lags + 1, 1)),
+        y = [1.96 / np.sqrt(len(data) - lag) for lag in range(0, lags + 1)],
+        color = 'red',
+        line_dash = 'dashed'
+    )
+
+    p1.line(
+        x = list(range(0, lags + 1, 1)),
+        y = [-1.96 / np.sqrt(len(data) - lag) for lag in range(0, lags + 1)],
+        color = 'red',
+        line_dash = 'dashed'
+    )
+
+    # P-Values from ljung box test
+    p1.line(
+        x = list(range(0, lags + 1, 1)),
+        y = pvalue,
+        color = 'green',
+        line_dash = 'dashed'
+    )
+
+    p1.line(
+        x = list(range(0, lags + 1, 1)),
+        y = [0.05] * (lags + 1),
+        color = 'green',
+        line_dash = 'dashed'
+    )
+
+    # PACF plot
+    p2 = figure(
+        title = 'PACF of BTC/USDT min data',
+        x_axis_label = 'Lags',
+        y_axis_label = 'Partial Autocorrelation',
+        y_range = [-1, 1],
+        height = 400,
+        width = 800
+    )
+
+    # PACF bars
+    p2.segment(
+        x0 = list(range(0, lags + 1, 1)),
+        x1 = list(range(0, lags + 1, 1)),
+        y0 = [0] * (lags + 1),
+        y1 = pacf,
+        color = 'blue',
+        line_width = 5
+    )
+
+    # At the 95% confidence interval z_(a-1)/sqrt(n - Li) a = 0.05, Li = Lag operator
+    p2.line(
+        x = list(range(0, lags + 1, 1)),
+        y = [1.96 / np.sqrt(len(data) - lag) for lag in range(0, lags + 1)],
+        color = 'red',
+        line_dash = 'dashed'
+    )
+
+    p2.line(
+        x = list(range(0, lags + 1, 1)),
+        y = [-1.96 / np.sqrt(len(data) - lag) for lag in range(0, lags + 1)],
+        color = 'red',
+        line_dash = 'dashed'
+    )
+
+    plot = gridplot([[p1],[p2]])
+    show(plot)
+
+def plot_predictions(test, prediction, bounds = False):
+    '''
+    Parameters:
+    test - Test data
+    prediction - Model predictions
+    bounds - List[[High], [Low]]
+    '''
+    p = figure(
+        title = 'Test vs Prediction',
+        x_axis_label = 'Time',
+        y_axis_label = 'Close Log Returns',
+        height = 600,
+        width = 600
+    )
+
+    p.line(
+        x = list(range(len(test))), 
+        y = test,
+        color = 'green'
+    )
+
+    p.line(
+        x = list(range(len(test))), 
+        y = prediction,
+        color = 'red'
+    )
+    if bounds != False:
+        p.line(
+            x = list(range(len(bounds[0]))), 
+            y = bounds[0],
+            color = 'red',
+            line_dash = 'dashed'
+        )
+
+        p.line(
+            x = list(range(len(bounds[1]))), 
+            y = bounds[1],
+            color = 'red',
+            line_dash = 'dashed'
+        )
+
+        p.patch(
+            x = [*list(range(len(bounds[0]))), *list(range(len(bounds[1])))[::-1]],
+            y = [*list(bounds[0]), *list(bounds[1])[::-1]],
+            color = 'red',
+            alpha = 0.2
+        )
+
+    show(p)
